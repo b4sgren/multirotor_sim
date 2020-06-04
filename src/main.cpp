df22ba4839e7b4b9a6d7a6c09062f749516806d7
Brendon Forsgren <realpepino@gmail.com>
2020-06-04 15:20:33 -0600

    Added the main file. Now to get something running

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "multirotor_sim/simulator.h"
+using namespace multirotor_sim;
+
+int main(int argc, char *argv[])
+{
+    // Simulator sim;
+    // sim.load("../params/sim_params.yaml");
+
+    // while (sim.run())
+    // {
+    //     ...
+    // }
+    return 0;
+}
