fefacfab087cac9bbf4713db46de71c167b9a011
Brendon Forsgren <realpepino@gmail.com>
2020-06-04 16:03:47 -0600

    Got the default controller to run. Now to simulate VINS Mono

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,11 @@ using namespace multirotor_sim;
 
 int main(int argc, char *argv[])
 {
-    // Simulator sim;
-    // sim.load("../params/sim_params.yaml");
+    Simulator sim;
+    sim.load("../params/sim_params.yaml");
 
-    // while (sim.run())
-    // {
-    //     ...
-    // }
+    while (sim.run())
+    {
+    }
     return 0;
 }
